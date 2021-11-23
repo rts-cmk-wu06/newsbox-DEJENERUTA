e328c2bf278d4023cbffe297d38a76973abae944
DEJENERUTA <dejenedaba269@gmail.com>
2021-11-23 12:18:46 +0100

    create slide to delete for archive page

--- js/archive.js
+++ js/archive.js
@@ -1,4 +1,88 @@
-if (localStorage.getItem("archiveCategory") === null) {
+let container = document.querySelector("#container");
+let actionButton = document.querySelector(".item button");
+let actionButtonWidth = actionButton.offsetWidth;
+let activeItem = null;
+let active = false;
+
+container.addEventListener("touchstart", dragStart, false);
+container.addEventListener("touchend", dragEnd, false);
+container.addEventListener("touchmove", drag, false);
+
+container.addEventListener("mousedown", dragStart, false);
+container.addEventListener("mouseup", dragEnd, false);
+container.addEventListener("mousemove", drag, false);
+
+[...document.querySelectorAll(".item button")].forEach(function (item) {
+  item.addEventListener("click", function () {
+    deleteItem(item.dataset.mid);
+  });
+});
+
+function dragStart(e) {
+  if (e.target !==e.currentTarget) {
+    active = true;
+    activeItem = e.target.closest(".item");
+
+    if (!activeItem !== null) {
+      if (!activeItem.xOffset) {
+        activeItem.xOffset = 0;
+      }
+      if (e.type === "touchstart") {
+        activeItem.initialX = e.touches[0] - activeItem.xOffset;
+      } else {
+        activeItem.initialX = e.clintX - activeItem.xOffset;
+      }
+    }
+  }
+}
+
+function dragEnd(e) {
+  if (activeItem !== null) {
+    activeItem.xOffset = 0;
+  }
+  if (activeItem.percentOpen > 75) {
+    setBack(-120, 0, activeItem);
+  } else {
+    setBack(0, 0, activeItem);
+  }
+  active = false;
+  activeItem = null;
+}
+
+function drag(e) {
+  if (active) {
+    e.preventDefault();
+    if (e.type === "touchmove") {
+      activeItem.currentX = e.touches[0].clientX - activeItem.initialX;
+    } else {
+      activeItem.currentX = e.clientX - activeItem.initialX;
+    }
+    activeItem.xOffset = activeItem.currentX;
+    activeItem.percentOpen = Math.round(
+      (Math.abs(activeItem.xOffset) / actionButtonWidth) * 100
+    );
+    setTranslate(activeItem.currentX, 0, activeItem);
+  }
+}
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+  el.style.webkitTransition = "";
+}
+function setBack(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+  el.style.webkitTransition = "transform .5s ease";
+}
+function deleteItem(item) {
+  var del = document.querySelector('.item[data-mid="' + item + '"');
+  del.style.display = "none";
+}
+
+
+
+
+
+/* if (localStorage.getItem("archiveCategory") === null) {
   localStorage.setItem(
     "archiveCategory",
     JSON.stringify({
@@ -7,4 +91,4 @@ if (localStorage.getItem("archiveCategory") === null) {
     })
   );
 }
-const archiveCategory = JSON.parse(localStorage.getItem("archiveCategory"));
+const archiveCategory = JSON.parse(localStorage.getItem("archiveCategory")); */
