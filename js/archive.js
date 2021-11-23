c492671c53524c18dd71450057baeea118332bcd
Dejene <dejenedaba269@gmail.com>
2021-11-23 08:21:20 +0100

    create localstorage for archive

--- js/archive.js
+++ js/archive.js
@@ -1,1 +1,10 @@
-
+if (localStorage.getItem("archiveCategory") === null) {
+  localStorage.setItem(
+    "archiveCategory",
+    JSON.stringify({
+      sport: true,
+      travel: true,
+    })
+  );
+}
+const archiveCategory = JSON.parse(localStorage.getItem("archiveCategory"));
